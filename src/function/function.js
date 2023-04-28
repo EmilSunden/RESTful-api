86b460392e6b894d6a1d7312e9fcb2b45c318dd7
EmilSunden <emilsunjobb@gmail.com>
2023-04-28 23:48:52 +0200

    Created databaseRunning function

--- src/function/function.js
+++ src/function/function.js
@@ -1,4 +1,10 @@
 const serverRunning = (port) => {
-    console.log(`Server running on ${port}`)
+    console.log(`Server running on ${port}.`)
 };
+
+const databaseRunning = () => {
+    console.log('Connected To Database.')
+}
+
 exports.serverRunning = serverRunning;
+exports.databaseRunning = databaseRunning;
